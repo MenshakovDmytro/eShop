e33c508a19bf2b93ee83b1620aad5b19dab4e87c
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-14 18:15:50 +0200

    added interfaces for Services

---
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogTypeService.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
+
+namespace Catalog.Host.Services.Interfaces
+{
+    public interface ICatalogTypeService
+    {
+        Task<int?> AddAsync(string name);
+        Task<ItemsListResponse<CatalogTypeDto>> GetTypesAsync();
+        Task<bool> RemoveAsync(string name);
+        Task<bool> UpdateAsync(string oldName, string newName);
+    }
+}
