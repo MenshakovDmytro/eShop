29644098ebb68aeaf3e34d6a1da0528fa4c55319
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-14 18:15:00 +0200

    created interfaces for Repos

---
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogBrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogBrandRepository
+    {
+        Task<int?> AddAsync(string name);
+        Task<bool> RemoveAsync(string name);
+        Task<bool> UpdateAsync(string oldName, string newName);
+        Task<ItemsList<CatalogBrand>> GetBrandsAsync();
+    }
+}
