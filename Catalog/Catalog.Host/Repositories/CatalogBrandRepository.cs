6f60087593d426a70334b459a1d20671c851c50e
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-14 18:15:11 +0200

    added Repos

---
+++ Catalog/Catalog.Host/Repositories/CatalogBrandRepository.cs
@@ -0,0 +1,75 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Host.Repositories
+{
+    public class CatalogBrandRepository : ICatalogBrandRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<CatalogBrandRepository> _logger;
+
+        public CatalogBrandRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<CatalogBrandRepository> logger)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+            _logger = logger;
+        }
+
+        public async Task<int?> AddAsync(string name)
+        {
+            var brand = await _dbContext.AddAsync(new CatalogBrand()
+            {
+                Brand = name
+            });
+
+            await _dbContext.SaveChangesAsync();
+
+            return brand.Entity.Id;
+        }
+
+        public async Task<ItemsList<CatalogBrand>> GetBrandsAsync()
+        {
+            var brands = await _dbContext.CatalogBrands
+                .ToListAsync();
+
+            return new ItemsList<CatalogBrand>() { TotalCount = brands.Count, Data = brands };
+        }
+
+        public async Task<bool> RemoveAsync(string name)
+        {
+            var brand = await _dbContext.CatalogBrands
+            .Where(w => w.Brand.Equals(name))
+            .FirstOrDefaultAsync();
+
+            if (brand is not null)
+            {
+                _dbContext.Remove(brand);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> UpdateAsync(string oldName, string newName)
+        {
+            var brand = await _dbContext.CatalogBrands
+            .Where(w => w.Brand.Equals(oldName))
+            .FirstOrDefaultAsync();
+
+            if (brand is not null)
+            {
+                brand.Brand = newName;
+
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Catalog/Catalog.Host/Repositories/CatalogTypeRepository.cs
@@ -0,0 +1,75 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Host.Repositories
+{
+    public class CatalogTypeRepository : ICatalogTypeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<CatalogBrandRepository> _logger;
+
+        public CatalogTypeRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<CatalogBrandRepository> logger)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+            _logger = logger;
+        }
+
+        public async Task<int?> AddAsync(string name)
+        {
+            var brand = await _dbContext.AddAsync(new CatalogType()
+            {
+                Type = name
+            });
+
+            await _dbContext.SaveChangesAsync();
+
+            return brand.Entity.Id;
+        }
+
+        public async Task<ItemsList<CatalogType>> GetTypesAsync()
+        {
+            var types = await _dbContext.CatalogTypes
+                .ToListAsync();
+
+            return new ItemsList<CatalogType>() { TotalCount = types.Count, Data = types };
+        }
+
+        public async Task<bool> RemoveAsync(string name)
+        {
+            var type = await _dbContext.CatalogTypes
+            .Where(w => w.Type.Equals(name))
+            .FirstOrDefaultAsync();
+
+            if (type is not null)
+            {
+                _dbContext.Remove(type);
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> UpdateAsync(string oldName, string newName)
+        {
+            var type = await _dbContext.CatalogTypes
+            .Where(w => w.Type.Equals(oldName))
+            .FirstOrDefaultAsync();
+
+            if (type is not null)
+            {
+                type.Type = newName;
+
+                await _dbContext.SaveChangesAsync();
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
