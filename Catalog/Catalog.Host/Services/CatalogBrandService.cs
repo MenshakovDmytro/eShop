7bfa9eafc0a7bd1855b35554b56ec23b8788f64f
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-17 20:10:31 +0200

    added Services

---
+++ Catalog/Catalog.Host/Services/CatalogBrandService.cs
@@ -0,0 +1,53 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+
+namespace Catalog.Host.Services
+{
+    public class CatalogBrandService : BaseDataService<ApplicationDbContext>, ICatalogBrandService
+    {
+        private readonly ICatalogBrandRepository _catalogBrandRepository;
+        private readonly IMapper _mapper;
+
+        public CatalogBrandService(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<BaseDataService<ApplicationDbContext>> logger,
+            ICatalogBrandRepository catalogBrandRepository,
+            IMapper mapper)
+            : base(dbContextWrapper, logger)
+        {
+            _catalogBrandRepository = catalogBrandRepository;
+            _mapper = mapper;
+        }
+
+        public Task<int?> AddAsync(string name)
+        {
+            return ExecuteSafeAsync(() => _catalogBrandRepository.AddAsync(name));
+        }
+
+        public async Task<ItemsListResponse<CatalogBrandDto>> GetBrandsAsync()
+        {
+            return await ExecuteSafeAsync(async () =>
+            {
+                var result = await _catalogBrandRepository.GetBrandsAsync();
+                return new ItemsListResponse<CatalogBrandDto>()
+                {
+                    Count = result.TotalCount,
+                    Data = result.Data.Select(s => _mapper.Map<CatalogBrandDto>(s)).ToList(),
+                };
+            });
+        }
+
+        public Task<int?> RemoveAsync(int id)
+        {
+            return ExecuteSafeAsync(() => _catalogBrandRepository.RemoveAsync(id));
+        }
+
+        public Task<int?> UpdateAsync(int id, string name)
+        {
+            return ExecuteSafeAsync(() => _catalogBrandRepository.UpdateAsync(id, name));
+        }
+    }
+}
--- Catalog/Catalog.Host/Services/CatalogItemService.cs
+++ Catalog/Catalog.Host/Services/CatalogItemService.cs
@@ -1,4 +1,6 @@
 using Catalog.Host.Data;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
 using Catalog.Host.Repositories.Interfaces;
 using Catalog.Host.Services.Interfaces;
 
@@ -7,18 +9,66 @@ namespace Catalog.Host.Services;
 public class CatalogItemService : BaseDataService<ApplicationDbContext>, ICatalogItemService
 {
     private readonly ICatalogItemRepository _catalogItemRepository;
+    private readonly IMapper _mapper;
 
     public CatalogItemService(
         IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
         ILogger<BaseDataService<ApplicationDbContext>> logger,
-        ICatalogItemRepository catalogItemRepository)
+        ICatalogItemRepository catalogItemRepository,
+        IMapper mapper)
         : base(dbContextWrapper, logger)
     {
         _catalogItemRepository = catalogItemRepository;
+        _mapper = mapper;
     }
 
     public Task<int?> AddAsync(string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName)
     {
-        return ExecuteSafeAsync(() => _catalogItemRepository.Add(name, description, price, availableStock, catalogBrandId, catalogTypeId, pictureFileName));
+        return ExecuteSafeAsync(() => _catalogItemRepository.AddAsync(name, description, price, availableStock, catalogBrandId, catalogTypeId, pictureFileName));
+    }
+
+    public async Task<ItemsListResponse<CatalogItemDto>> GetByBrandAsync(string brand)
+    {
+        return await ExecuteSafeAsync(async () =>
+        {
+            var result = await _catalogItemRepository.GetByBrandAsync(brand);
+            return new ItemsListResponse<CatalogItemDto>()
+            {
+                Count = result.TotalCount,
+                Data = result.Data.Select(s => _mapper.Map<CatalogItemDto>(s)).ToList(),
+            };
+        });
+    }
+
+    public async Task<CatalogItemDto?> GetByIdAsync(int id)
+    {
+        return await ExecuteSafeAsync(async () =>
+        {
+            var result = await _catalogItemRepository.GetByIdAsync(id);
+            return _mapper.Map<CatalogItemDto?>(result);
+        });
+    }
+
+    public async Task<ItemsListResponse<CatalogItemDto>> GetByTypeAsync(string type)
+    {
+        return await ExecuteSafeAsync(async () =>
+        {
+            var result = await _catalogItemRepository.GetByTypeAsync(type);
+            return new ItemsListResponse<CatalogItemDto>()
+            {
+                Count = result.TotalCount,
+                Data = result.Data.Select(s => _mapper.Map<CatalogItemDto>(s)).ToList(),
+            };
+        });
+    }
+
+    public Task<int?> RemoveAsync(int id)
+    {
+        return ExecuteSafeAsync(() => _catalogItemRepository.RemoveAsync(id));
+    }
+
+    public Task<int?> UpdateAsync(int id, string name, string description, decimal price, int availableStock, int catalogBrandId, int catalogTypeId, string pictureFileName)
+    {
+        return ExecuteSafeAsync(() => _catalogItemRepository.UpdateAsync(id, name, description, price, availableStock, catalogBrandId, catalogTypeId, pictureFileName));
     }
 }
