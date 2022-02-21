0084a8763c0a55a269c0599e3ba431039801aa32
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-21 18:39:21 +0200

    added Interfaces for Brand and Type Repositories

---
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogBrandRepository.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogBrandRepository
+    {
+        Task<ItemsList<CatalogBrand>> GetBrandsAsync();
+    }
+}
---
+++ Catalog/Catalog.Host/Repositories/Interfaces/ICatalogTypeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+
+namespace Catalog.Host.Repositories.Interfaces
+{
+    public interface ICatalogTypeRepository
+    {
+        Task<ItemsList<CatalogType>> GetTypesAsync();
+    }
+}
