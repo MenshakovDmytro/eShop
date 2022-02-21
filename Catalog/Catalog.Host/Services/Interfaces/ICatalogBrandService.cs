c3c007a256887297de122b58a482989b9f7a1023
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-21 18:39:53 +0200

    added Brand and Type Services

---
+++ Catalog/Catalog.Host/Services/Interfaces/ICatalogBrandService.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
+
+namespace Catalog.Host.Services.Interfaces
+{
+    public interface ICatalogBrandService
+    {
+        Task<ItemsListResponse<CatalogBrandDto>> GetBrandsAsync();
+    }
+}
