8951ded2536e65a0ca76f7aeefe7c8848c76f3cf
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-21 18:39:38 +0200

    added Brand and Type Repositories

---
+++ Catalog/Catalog.Host/Repositories/CatalogBrandRepository.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Repositories.Interfaces;
+
+namespace Catalog.Host.Repositories
+{
+    public class CatalogBrandRepository : ICatalogBrandRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<CatalogBrandRepository> _logger;
+
+        public CatalogBrandRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<CatalogBrandRepository> logger)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+            _logger = logger;
+        }
+
+        public async Task<ItemsList<CatalogBrand>> GetBrandsAsync()
+        {
+            var brands = await _dbContext.CatalogBrands
+                .ToListAsync();
+
+            return new ItemsList<CatalogBrand>() { TotalCount = brands.Count, Data = brands };
+        }
+    }
+}
---
+++ Catalog/Catalog.Host/Repositories/CatalogTypeRepository.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Data.Entities;
+using Catalog.Host.Repositories.Interfaces;
+
+namespace Catalog.Host.Repositories
+{
+    public class CatalogTypeRepository : ICatalogTypeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private readonly ILogger<CatalogBrandRepository> _logger;
+
+        public CatalogTypeRepository(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<CatalogBrandRepository> logger)
+        {
+            _dbContext = dbContextWrapper.DbContext;
+            _logger = logger;
+        }
+
+        public async Task<ItemsList<CatalogType>> GetTypesAsync()
+        {
+            var types = await _dbContext.CatalogTypes
+                .ToListAsync();
+
+            return new ItemsList<CatalogType>() { TotalCount = types.Count, Data = types };
+        }
+    }
+}
