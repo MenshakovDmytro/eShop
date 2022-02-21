7c14a9439ed3acefbfec46e8b08b1d3e21c1391a
MenshakovDmytro <jgadf1kz@gmail.com>
2022-02-21 18:40:35 +0200

    added Brand and Type Service classes

---
+++ Catalog/Catalog.Host/Services/CatalogBrandService.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+
+namespace Catalog.Host.Services
+{
+    public class CatalogBrandService : BaseDataService<ApplicationDbContext>, ICatalogBrandService
+    {
+        private readonly ICatalogBrandRepository _catalogBrandRepository;
+        private readonly IMapper _mapper;
+
+        public CatalogBrandService(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<BaseDataService<ApplicationDbContext>> logger,
+            ICatalogBrandRepository catalogBrandRepository,
+            IMapper mapper)
+            : base(dbContextWrapper, logger)
+        {
+            _catalogBrandRepository = catalogBrandRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ItemsListResponse<CatalogBrandDto>> GetBrandsAsync()
+        {
+            return await ExecuteSafeAsync(async () =>
+            {
+                var result = await _catalogBrandRepository.GetBrandsAsync();
+                return new ItemsListResponse<CatalogBrandDto>()
+                {
+                    Count = result.TotalCount,
+                    Data = result.Data.Select(s => _mapper.Map<CatalogBrandDto>(s)).ToList(),
+                };
+            });
+        }
+    }
+}
---
+++ Catalog/Catalog.Host/Services/CatalogTypeService.cs
@@ -0,0 +1,38 @@
+﻿using Catalog.Host.Data;
+using Catalog.Host.Models.Dtos;
+using Catalog.Host.Models.Response;
+using Catalog.Host.Repositories.Interfaces;
+using Catalog.Host.Services.Interfaces;
+
+namespace Catalog.Host.Services
+{
+    public class CatalogTypeService : BaseDataService<ApplicationDbContext>, ICatalogTypeService
+    {
+        private readonly ICatalogTypeRepository _catalogTypeRepository;
+        private readonly IMapper _mapper;
+
+        public CatalogTypeService(
+            IDbContextWrapper<ApplicationDbContext> dbContextWrapper,
+            ILogger<BaseDataService<ApplicationDbContext>> logger,
+            ICatalogTypeRepository catalogTypeRepository,
+            IMapper mapper)
+            : base(dbContextWrapper, logger)
+        {
+            _catalogTypeRepository = catalogTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ItemsListResponse<CatalogTypeDto>> GetTypesAsync()
+        {
+            return await ExecuteSafeAsync(async () =>
+            {
+                var result = await _catalogTypeRepository.GetTypesAsync();
+                return new ItemsListResponse<CatalogTypeDto>()
+                {
+                    Count = result.TotalCount,
+                    Data = result.Data.Select(s => _mapper.Map<CatalogTypeDto>(s)).ToList(),
+                };
+            });
+        }
+    }
+}
